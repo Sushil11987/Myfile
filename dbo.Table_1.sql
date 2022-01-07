f7c6287d3174548bc81734f2bc1d1f3ef5b628bc
Sushil11987 <35392187+Sushil11987@users.noreply.github.com>
2022-01-07 20:51:19 +0530

    DBTB

---
+++ dbo.Table_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table2]
+(
+	[Name] NVARCHAR(50) NOT NULL , 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [Subject] NVARCHAR(50) NOT NULL, 
+    [Message] NVARCHAR(500) NOT NULL, 
+    CONSTRAINT [PK_Table2] PRIMARY KEY ([Email])
+)
